388fe07db2e4799438ecceed79489266ce70f7ef
Rob Boyer <rboyer@comcast.net>
2022-01-23 16:33:03 -0500

    Add port parameter to launcher script.

--- launch.sh
+++ launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./launch.sh <server ip>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./launch.sh <server hostname/ip> <port>"
     exit 1
 fi
 
 # Generate token and securely write to server token file.
 token_file="/tmp/pi-server-token"
 token=$(openssl rand -base64 12)
-echo -n "$token" | ssh pi@"$1" -T "cat > $token_file"
+echo -n "$token" | ssh pi@"$1" -p "$2" -T "cat > $token_file"
 
 # Launch pi-client with same token.
 ./pi-client "$1" "$token"
